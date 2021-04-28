0d8eb404964b004629b0d176a9a956c513862126
Stefan Kinauer <stefan.kinauer@jambit.com>
2021-04-28 15:14:02 +0200

    Parametrizing number of games to play, time measurement

--- run_competition.py
+++ run_competition.py
@@ -1,15 +1,20 @@
 from src import Competition
 from src.agents import RandomAgent, CalculatorAgent
+from time import time
 
 if __name__ == '__main__':
-    competition = Competition()
+    num_games = 10000
+    competition = Competition(num_games)
 
     randy = RandomAgent()
     sheldon = CalculatorAgent()
     competition.registerParticipant(randy)
     competition.registerParticipant(sheldon)
 
+    start_time = time()
     competition.startCompetition()
+    time_used = time() - start_time
+    print(f'competition took {time_used} seconds. On average this is {time_used / num_games} seconds.')
     competition.printResult()
     # competition.plot_score_history()
     competition.plot_histogram()
