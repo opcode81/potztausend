33717e393f7e84bc44947deedb2026009d7db61f
Dominik Jain <dominik.jain@jambit.com>
2021-05-04 18:15:53 +0200

    Refactoring, drastically improving (cache) efficiency

--- src/agents/util_agent.py
+++ src/agents/util_agent.py
@@ -1,7 +1,11 @@
-from .agent import Agent, MatrixColumn
-import numpy as np
-from functools import lru_cache
 import random
+from functools import lru_cache
+from typing import Tuple
+
+import numpy as np
+
+from .agent import Agent, MatrixColumn
+from .. import competition
 
 
 class UtilAgent(Agent):
@@ -9,8 +13,8 @@ class UtilAgent(Agent):
     A utilitarian agent, named after Jeremy Bentham, the father of modern utilitarianism.
     It maximises the expected utility according to the rules of the game (-1000 for overshoot).
     """
-    _factors = np.array([100, 10, 1])
-    OVERSHOOT_REWARD = -1000
+    FACTORS = np.array([100, 10, 1])
+    DICE_VALUES = [1, 2, 3, 4, 5, 6]
 
     def __init__(self):
         super().__init__("Jeremy")
@@ -32,58 +36,38 @@ class UtilAgent(Agent):
         self.value_matrix[action] += diceValue
         return MatrixColumn(action)
 
-    def _evaluate_action(self, diceValue, action_index) -> float:  # returns the utility of the action
-        old_value = self._eval(self.value_matrix)
-        added_value = self._factors[action_index] * diceValue
+    def _evaluate_action(self, diceValue, action_index) -> float:
+        old_value = np.dot(self.value_matrix, self.FACTORS)
+        added_value = self.FACTORS[action_index] * diceValue
         future_value = old_value + added_value
-        gap = 1000 - future_value
-        test_occupancy = self.occupancy.copy()
-        test_occupancy[action_index] += 1
-        return self.eval_utility(UtilAgent.__concat_args(test_occupancy, gap), added_value)
+        resulting_occupancy = self.occupancy.copy()
+        resulting_occupancy[action_index] += 1
+        return self._eval_utility(tuple(resulting_occupancy), future_value)
 
     @lru_cache(maxsize=None)
-    def eval_utility(self, occupancy_gap, util) -> float:
-        occupancy = np.array(occupancy_gap[:3])
-        gap = occupancy_gap[3]
-
+    def _eval_utility(self, occupancy: Tuple[int, int, int], util) -> float:
+        gap = 1000 - util
         if gap < 0:
-            return self.OVERSHOOT_REWARD
+            return competition.OVERSHOOT_SCORE
 
+        occupancy = np.array(occupancy)
         available_columns = np.where(occupancy < 3)[0]
 
         if len(available_columns) == 0:
             return util
 
-        lower_bound = np.dot((3-occupancy), self._factors)
+        lower_bound = np.dot((3-occupancy), self.FACTORS)
         if gap < lower_bound:
-            return self.OVERSHOOT_REWARD
+            return competition.OVERSHOOT_SCORE
 
-        possible_throws = np.array([1,2,3,4,5,6])
-
-        util_per_throw = []
-        for throw in possible_throws: # choose last to fill column
-            throw_util = np.ones(3, float) * self.OVERSHOOT_REWARD
+        next_dice_value_utils = []
+        for dice_value in self.DICE_VALUES:
+            column_util = np.ones(3, float) * competition.OVERSHOOT_SCORE
             for c in available_columns:
-                test_occupancy = occupancy.copy()
-                test_occupancy[c] += 1
-                added_value = self._factors[c] * throw
-                u = self.eval_utility(UtilAgent.__concat_args(test_occupancy, gap - added_value), util + added_value)
-                throw_util[c] = u
-            util_per_throw.append(np.max(throw_util))
-        mean_util = np.mean(np.array(util_per_throw))
+                next_occupancy = occupancy.copy()
+                next_occupancy[c] += 1
+                added_value = self.FACTORS[c] * dice_value
+                column_util[c] = self._eval_utility(tuple(next_occupancy), util + added_value)
+            next_dice_value_utils.append(np.max(column_util))
+        mean_util = np.mean(next_dice_value_utils)
         return mean_util
-
-    @staticmethod
-    def __concat_args(occ, gap):
-        return (occ[0], occ[1], occ[2], gap)
-
-    def _eval(self, matrix=None):
-        if matrix is None:
-            return np.dot(self.value_matrix, self._factors)
-        else:
-            return np.dot(matrix, self._factors)
-
-    def _delta(self, index, value):
-        mat = np.zeros(3, dtype=int)
-        mat[index] = value
-        return mat
