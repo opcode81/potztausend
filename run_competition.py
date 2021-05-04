b8b0709d6ed5bcb17e47f1cfb157dbf9adf5f3a7
Dominik Jain <dominik.jain@jambit.com>
2021-05-04 16:37:54 +0200

    Added utilitarian agent Jeremy, print mean score achieved

--- run_competition.py
+++ run_competition.py
@@ -1,9 +1,19 @@
+import logging
+from pprint import pprint
+import sys
+
+import numpy as np
+
 from src import Competition
 from src.agents import RandomAgent, CalculatorAgent, QLearningAgent, TemporalDifferenceAgent, ValueIterationAgent
 from time import time
 
+from src.agents.util_agent import UtilAgent
+
+
 if __name__ == '__main__':
-    num_games = 10000
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)-5s %(asctime)-15s %(name)s:%(funcName)s - %(message)s', stream=sys.stdout)
+    num_games = 1000
     competition = Competition(num_games)
 
     randy = RandomAgent()
@@ -11,11 +21,13 @@ if __name__ == '__main__':
     paul = QLearningAgent()
     gunter = TemporalDifferenceAgent()
     lotte = ValueIterationAgent()
-    competition.registerParticipant(randy)
+    jeremy = UtilAgent()
+    #competition.registerParticipant(randy)
     competition.registerParticipant(sheldon)
     competition.registerParticipant(paul)
-    competition.registerParticipant(gunter)
-    competition.registerParticipant(lotte)
+    #competition.registerParticipant(gunter)
+    #competition.registerParticipant(lotte)
+    competition.registerParticipant(jeremy)
 
     start_time = time()
     competition.startCompetition()
@@ -24,3 +36,6 @@ if __name__ == '__main__':
     competition.printResult()
     # competition.plot_score_history()
     competition.plot_histogram()
+
+    mean_scores = {p.agent.agentName: np.mean([-1000 if s > 1000 else s for s in p.score_history]) for p in competition.participants}
+    pprint(mean_scores, width=1)
