a487264deba2825a5d1628b82e238700eeb2f87c
Dominik Jain <dominik.jain@jambit.com>
2022-08-18 18:08:55 +0200

    Added support for fallback action when selecting forbidden action; added PPOAgent

--- src/agents/deep_rl_agent.py
+++ src/agents/deep_rl_agent.py
@@ -1,14 +1,17 @@
+import logging
 import os
 from abc import ABC, abstractmethod
 
 import gym
 import numpy as np
-from stable_baselines3 import A2C, DQN
+from stable_baselines3 import A2C, DQN, PPO
 from stable_baselines3.common.base_class import BaseAlgorithm
 
 import competition
 from agents import MatrixColumn, Agent
 from competition import GameState, Competition
+
+log = logging.getLogger(__name__)
 
 
 class Env(gym.Env):
@@ -17,7 +20,7 @@ class Env(gym.Env):
     def __init__(self):
         self.state = GameState()
         self.diceValue = 0
-        self.observation_space = gym.spaces.Box(-1.0, 1.0, shape=[6])
+        self.observation_space = gym.spaces.Box(-1.0, 1.0, shape=[8])
         self.rand = np.random.RandomState()
         self.action_space = gym.spaces.Discrete(3)
 
@@ -30,7 +33,7 @@ class Env(gym.Env):
         s = self.state.calcSum()
         resultingValues = [s + self.diceValue, s + self.diceValue*10, s + self.diceValue*100]
         resultingValuesNorm = [(x - target) / target for x in resultingValues]
-        return np.array([*resultingValuesNorm, *remainingActionsPerCol])
+        return np.array([s/target, diceValue/6, *resultingValuesNorm, *remainingActionsPerCol])
 
     def reset(self):
         self.state = GameState()
@@ -63,7 +66,16 @@ class Env(gym.Env):
 
 
 class DeepRLAgent(Agent, ABC):
-    def __init__(self, load: bool, filebasename: str):
+    def __init__(self, load: bool, filebasename: str, useFallbackForForbiddenActions=False):
+        """
+        :param load: whether to load a previously stored model
+        :param filebasename: the base filename for storage
+        :param useFallbackForForbiddenActions: whether to use, in cases where the policy selects an inadmissible action,
+            the policy's probabilistic outputs to select a fallback action (the most probable admissible action).
+            This option is useful for agents that have not been trained a lot; it will allow them to achieve better
+            performance.
+        """
+        self.useFallbackForForbiddenActions = useFallbackForForbiddenActions
         self.env = Env()
         self.path = os.path.join("model_resources", "deep_rl", f"{filebasename}.zip")
         self.model = self._createModel(self.env)
@@ -85,14 +97,28 @@ class DeepRLAgent(Agent, ABC):
         self.env.reset()
 
     def doMove(self, diceValue) -> MatrixColumn:
-        action, _ = self.model.predict(self.env.get_obs(diceValue), deterministic=True)
-        self.env.step(action)
-        return MatrixColumn(action)
+        obs = self.env.get_obs(diceValue)
+        if self.useFallbackForForbiddenActions:
+            policy = self.model.policy
+            obs, vectorized_env = policy.obs_to_tensor(obs)
+            dist = self.model.policy.get_distribution(obs)
+            actions = list(MatrixColumn)
+            probs = dist.distribution.probs.tolist()[0]
+            ap = sorted(zip(actions, range(len(actions)), probs), key=lambda x: -x[2])
+            possibleActions = self.env.state.getPossibleActions()
+            for action, actionIdx, p in ap:
+                if action in possibleActions:
+                    break
+        else:
+            actionIdx, _ = self.model.predict(obs, deterministic=True)
+            action = MatrixColumn(actionIdx)
+        self.env.step(actionIdx)
+        return action
 
 
 class A2CAgent(DeepRLAgent):
-    def __init__(self, load=False):
-        super().__init__(load, "a2c")
+    def __init__(self, load=False, useFallbackForForbiddenActions=False):
+        super().__init__(load, "a2c", useFallbackForForbiddenActions=useFallbackForForbiddenActions)
 
     def _createModel(self, env) -> BaseAlgorithm:
         return A2C('MlpPolicy', env, verbose=1)
@@ -106,11 +132,23 @@ class DQNAgent(DeepRLAgent):
         return DQN('MlpPolicy', env, verbose=1)
 
 
-if __name__ == '__main__':
-    #agent = A2CAgent()
-    agent = DQNAgent()
+class PPOAgent(DeepRLAgent):
+    def __init__(self, load=False):
+        super().__init__(load, "ppo")
 
-    agent.train(1000000)
+    def _createModel(self, env) -> BaseAlgorithm:
+        return PPO('MlpPolicy', env, verbose=1)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+
+    #agent = DQNAgent()
+    agent = A2CAgent()
+    #agent = PPOAgent()
+    #agent = A2CAgent(load=True)
+
+    agent.train(3000000)
     #agent.save()
 
     comp = Competition(numberOfGames=1000)
